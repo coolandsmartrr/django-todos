2618e1d8a175ca533298ff91a9bc3253c16beaa8
Richard Rowland <coolandsmartrr@gmail.com>
2024-05-31 14:10:19 +0900

    Created ability to add todo from main view

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def todo_list(request):
+  if request.method == 'POST':
+    form = TodoForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('todo_list')
+  else:
+    form = TodoForm()
+  
   todos = Todo.objects.all()
-  return render(request, 'todo_list.html', {'todos': todos})
+  return render(request, 'todo_list.html', {'todos': todos, 'form': form})
