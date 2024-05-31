32898a38b07f5ffab4ecabc1da00f26c8192d096
Richard Rowland <coolandsmartrr@gmail.com>
2024-05-31 14:20:44 +0900

    Added feature to delete todo item

--- todos/views.py
+++ todos/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from .forms import TodoForm
 
 # Create your views here.
 def todo_list(request):
   if request.method == 'POST':
-    form = TodoForm(request.POST)
-    if form.is_valid():
-      form.save()
+    if 'add_todo' in request.POST:
+      form = TodoForm(request.POST)
+      if form.is_valid():
+        form.save()
+        return redirect('todo_list')
+    if 'delete_todo' in request.POST:
+      todo_id = request.POST.get('todo_id')
+      todo = get_object_or_404(Todo, id=todo_id)
+      todo.delete()
       return redirect('todo_list')
   else:
     form = TodoForm()
